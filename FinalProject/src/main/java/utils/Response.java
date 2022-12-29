74d4876a3bb5bab575869edd92322f20b47eef32
vladiodes <vladiodes@gmail.com>
2022-12-29 21:34:48 +0200

    Adding a response object for communication between backend and frontend

---
+++ FinalProject/src/main/java/utils/Response.java
@@ -0,0 +1,33 @@
+package utils;
+
+public class Response<T> {
+    private T value;
+    private String msg;
+
+    public static <T> Response<T> OK(T value){
+        return new Response<>(value);
+    }
+
+    public static <T> Response<T> OK(T value, String msg){
+        return new Response<>(value,msg);
+    }
+
+    public static <T> Response<T> FAIL(Exception exception){
+        return new Response<>(exception);
+    }
+
+    private Response(T value){
+        this.value = value;
+    }
+
+    private Response(T value, String msg){
+        this.value = value;
+        this.msg = msg;
+    }
+
+    private Response(Exception exception){
+        this.msg = exception.getMessage();
+    }
+
+
+}
