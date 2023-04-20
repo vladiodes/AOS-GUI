b0a4a0085aa64f93b438f97f7de18c8a23932f46
shahar <lankrys@post.bgu.ac.il>
2023-04-20 04:30:07 -0700

    get execution outcome request implementation

--- FinalProject/src/main/java/DTO/HttpRequests/GetExecutionOutcomeRequestDTO.java
+++ FinalProject/src/main/java/DTO/HttpRequests/GetExecutionOutcomeRequestDTO.java
@@ -3,14 +3,19 @@ package DTO.HttpRequests;
 import backend.finalproject.IntegrationRequests.IntegrationRequestsHandler;
 import utils.IntegrationRequestResponse;
 
-public class StopRobotRequestDTO implements HttpRequestDTO{
+public class GetExecutionOutcomeRequestDTO implements HttpRequestDTO{
 
-    public StopRobotRequestDTO(){
-
+    private final int belief_size;
+    public GetExecutionOutcomeRequestDTO(int belief_size){
+        this.belief_size = belief_size;
     }
     @Override
     public IntegrationRequestResponse visit(IntegrationRequestsHandler handler) {
         return handler.handle(this);
+    }
+
+    public int getBeliefSize() {
+        return belief_size;
     }
 
 }
