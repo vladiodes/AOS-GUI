68670b28149a4f898e535c72c7be2b22838245c4
vladiodes <vladiodes@gmail.com>
2023-05-31 13:42:15 +0300

    Updated the toy_state_displayer script

--- StateDisplayers/toys_displayer.py
+++ StateDisplayers/toys_displayer.py
@@ -1,63 +1,66 @@
 from PIL import Image, ImageDraw
-import matplotlib.pyplot as plt
 
-def display(game_state, filename = None,is_merging_images = False):
-    # Define the dimensions and positions
-    width = 700
-    height = 400
-    padding = 30
-    location_width = (width - padding * 2) / 5
-    toy_size = min(location_width, 80)
-    text_padding = 10
-    label_spacing = 5
+def display(state, filename = None,is_merging_images = False):
+    # Define cell and image dimensions
+    cell_size = 80
+    num_rows = 6
+    num_cols = 6
+    image_width = num_cols * cell_size
+    image_height = num_rows * cell_size
 
     # Create a blank image
-    image = Image.new('RGB', (width, height), color='white')
+    image = Image.new("RGB", (image_width, image_height), "white")
     draw = ImageDraw.Draw(image)
 
-    # Draw the locations and their contents
-    for i, location in enumerate(range(1, 6)):
-        x = padding + i * location_width
-        y = padding + toy_size + text_padding
-        draw.rectangle([x, y, x + location_width, y + toy_size], outline='black')
-        draw.text((x + location_width / 2, y + toy_size / 2 - toy_size / 4 - text_padding), f"Location {location}", fill='black', anchor='middle')
+    # Define toy colors
+    toy_colors = {
+        "red": (255, 0, 0),
+        "black": (0, 0, 0),
+        "blue": (0, 0, 255),
+        "green": (0, 255, 0)
+    }
 
-        toys_at_location = []
-        for toy in ['toy1', 'toy2', 'toy3', 'toy4']:
-            if game_state[toy]['location'] == location:
-                toys_at_location.append((toy, game_state[toy]['type']))
 
-        if toys_at_location:
-            toy_height = toy_size / (len(toys_at_location) + 1)
-            for j, (toy, color) in enumerate(toys_at_location):
-                toy_y = y + toy_height * (j + 1)
-                draw.text((x + location_width / 2, toy_y - toy_height / 2 - text_padding), toy, fill=color, anchor='middle')
-        else:
-            draw.text((x + location_width / 2, y + toy_size / 2 - toy_size / 4 + label_spacing), 'Empty', fill='black', anchor='middle')
+    # Draw labels for toy rows
+    for row in range(num_rows - 1):
+        toy_key = f"toy{row + 1}"
+        if toy_key in state:
+            toy = state[toy_key]
+            toy_type = toy["type"]
+            label = f"Toy{row + 1}: {toy_type.capitalize()}"
+            draw.text((10, row * cell_size + 10), label, fill=(0, 0, 0))
 
-    # Draw the robot's location
-    robot_x = padding + (game_state['robotLocation'] - 1) * location_width + location_width / 2
-    robot_y = padding
-    robot_size = toy_size / 2
-    robot_polygon = [(robot_x - robot_size, robot_y + robot_size), (robot_x + robot_size, robot_y + robot_size), (robot_x, robot_y)]
-    draw.polygon(robot_polygon, fill='blue')
+    # Draw label for robot row
+    draw.text((10, (num_rows - 1) * cell_size + 10), "Robot", fill=(0, 0, 0))
 
-    # Draw the 'robot' label
-    draw.text((robot_x, robot_y + robot_size + text_padding), 'Robot', fill='black', anchor='middle')
+    # Draw labels for location columns
+    for col in range(num_cols):
+        label = f"Location {col}"
+        draw.text((col * cell_size + 10, (num_rows - 1) * cell_size - 30), label, fill=(0, 0, 0))
 
-    # Draw the robot's arm position
-    arm_x = padding + (game_state['robotArm'] - 1) * location_width + location_width / 2
-    arm_y = robot_y + robot_size + 2 * text_padding
-    draw.rectangle([arm_x - 10, arm_y, arm_x + 10, arm_y + 30], fill='gray', outline='black')
-    draw.text((arm_x, arm_y + 15), 'Arm', fill='black', anchor='middle')
+    # Draw rectangles for toys
+    for row in range(num_rows - 1):
+        toy_key = f"toy{row + 1}"
+        if toy_key in state:
+            toy = state[toy_key]
+            toy_location = toy["location"]
+            toy_type = toy["type"]
+            toy_color = toy_colors[toy_type]
+            x1 = toy_location * cell_size
+            y1 = row * cell_size
+            x2 = (toy_location + 1) * cell_size
+            y2 = (row + 1) * cell_size
+            draw.rectangle([x1, y1, x2, y2], fill=toy_color)
 
-    # Draw the pick actions left
-    draw.text((padding, height - padding - toy_size - 70), f"Pick Actions Left: {game_state['pickActionsLeft']}", fill='black')
+    # Draw circle for robot
+    robot_location = state["robotLocation"]
+    x1 = robot_location * cell_size + cell_size // 4
+    y1 = (num_rows - 1) * cell_size + cell_size // 4
+    x2 = (robot_location + 1) * cell_size - cell_size // 4
+    y2 = num_rows * cell_size - cell_size // 4
+    draw.ellipse([x1, y1, x2, y2], fill=(0, 0, 0))
 
-    # Show the image
-    
-
-        # Show the plot
+                # Show the plot
     if not is_merging_images:
         image.show()
     else:
